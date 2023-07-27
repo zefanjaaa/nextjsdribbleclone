9ac81f7e2e15248199a88257ab3d688aa09eae22
Zefanja Timorason <z.timorason@gmail.com>
2023-07-27 18:56:17 +0200

    modeling

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,31 @@
+import { g, auth, config } from "@grafbase/sdk";
+
+// Welcome to Grafbase!
+// Define your data models, integrate auth, permission rules, custom resolvers, search, and more with Grafbase.
+// Integrate Auth
+// https://grafbase.com/docs/auth
+//
+
+const User = g.model("User", {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
+  projects: g.relation(() => Project).list(),
+});
+
+const Project = g.model("Project", {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url().optional(),
+  githubUrl: g.url().optional(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+});
+
+export default config({
+  schema: g,
+});
