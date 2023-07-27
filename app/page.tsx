2bc135ffbb84f617199906fb02c693acdee8f74d
Zefanja Timorason <z.timorason@gmail.com>
2023-07-27 16:44:02 +0200

    inital homepage setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="flex-start flex-col mb-16 paddings">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Loadmore</h1>
+    </section>
   );
 };
 
