0e4c064bd44fec59329e3672972430d055d66a8b
Zefanja Timorason <z.timorason@gmail.com>
2023-08-07 14:41:06 +0200

    created a custom button,custom menu and forms

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,21 +1,130 @@
 "use client";
-import { SessionInterface } from "@/common.types";
+
+import Image from "next/image";
+import React, { ChangeEvent, useState, FormEvent } from "react";
+import FormField from "./FormField";
+import { categoryFilters } from "@/constants";
+import CustomMenu from "./CustomMenu";
+import { FormState, ProjectInterface, SessionInterface } from "@/common.types";
+import { useRouter } from "next/navigation";
+import Button from "./Button";
 
 type Props = {
   type: string;
   session: SessionInterface;
+  project?: ProjectInterface;
 };
 
-const image = null;
+const ProjectForm = ({ type, session, project }: Props) => {
+  const router = useRouter();
 
-const ProjectForm = ({ type, session }: Props) => {
+  const [submitting, setSubmitting] = useState(false);
+  const [form, setForm] = useState<FormState>({
+    title: project?.title || "",
+    description: project?.description || "",
+    image: project?.image || "",
+    liveSiteUrl: project?.liveSiteUrl || "",
+    githubUrl: project?.githubUrl || "",
+    category: project?.category || "",
+  });
   const handleSubmit = (event: React.FormEvent) => {};
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    if (!file.type.includes("image")) {
+      alert("Please upload an image!");
+
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const result = reader.result as string;
+
+      handleStateChange("image", result);
+    };
+  };
+  const handleStateChange = (fieldName: string, value: string) => {
+    setForm((prevState) => ({
+      ...prevState,
+      [fieldName]: value,
+    }));
+  };
+
   return (
     <form onSubmit={handleSubmit} className="flexStart form">
       <div className="flexStart form_image-container">
         <label htmlFor="poster" className="flexCenter form_image-label">
-          {!image && "Choose a poster"}
+          {form.image && "Choose a poster"}
         </label>
+        <input
+          type="file"
+          id="image"
+          accept="image/*"
+          required={type === "create" ? true : false}
+          className="form_image-input"
+          onChange={handleChangeImage}
+        />
+        {form.image && (
+          <Image
+            src={form?.image}
+            className="sm:p-10 object-contain z-20"
+            alt="project post"
+            fill
+          />
+        )}
+      </div>
+      <FormField
+        title="Title"
+        state={form.title}
+        placeholder="text"
+        setState={(value) => handleStateChange("title", value)}
+      />
+      <FormField
+        title="Description"
+        state={form.description}
+        placeholder="Showcase your project"
+        setState={(value) => handleStateChange("description", value)}
+      />
+      <FormField
+        type="url"
+        title="Website URL"
+        state={form.liveSiteUrl}
+        placeholder="http://localhost:3000/"
+        setState={(value) => handleStateChange("liveSiteUrl", value)}
+      />
+      <FormField
+        type="url"
+        title="GitHub URL"
+        state={form.githubUrl}
+        placeholder="https://github.com/zefanjaaa"
+        setState={(value) => handleStateChange("githubUrl", value)}
+      />
+      <CustomMenu
+        title="Category"
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value) => handleStateChange("category", value)}
+      />
+      <div className="flexStart w-full">
+        <Button
+          title={
+            submitting
+              ? `${type === "create" ? "Creating" : "Editing"}`
+              : `${type === "create" ? "Create" : "Edit"}`
+          }
+          type="submit"
+          leftIcon={submitting ? "" : "/plus.svg"}
+          submitting={submitting}
+        />
       </div>
     </form>
   );
